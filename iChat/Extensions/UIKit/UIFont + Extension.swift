d4fd27f1ccc60c8976ce11bacc530ff49876eb39
xubuntus <nikita.dmd@gmail.com>
2024-02-13 00:14:03 +0300

    Add assets and UIKit extensions

---
+++ iChat/Extensions/UIKit/UIFont + Extension.swift
@@ -0,0 +1,19 @@
+//
+//  UIFont + Extension.swift
+//  iChat
+//
+//  Created by nik on 10.02.2024.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    static func avenir20() -> UIFont? {
+        UIFont.init(name: "avenir", size: 20)
+    }
+    
+    static func avenir26() -> UIFont? {
+        UIFont.init(name: "avenir", size: 26)
+    }
+}
---
+++ iChat/Extensions/UIKit/UILabel + Extension.swift
@@ -0,0 +1,17 @@
+//
+//  UILabel + Extension.swift
+//  iChat
+//
+//  Created by nik on 10.02.2024.
+//
+
+import UIKit
+
+extension UILabel {
+    
+    convenience init(text: String) {
+        self.init()
+        
+        self.text = text
+    }
+}
