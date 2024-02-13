a8581dd97d190c86a9f6c06e2266e58140cde0af
xubuntus <nikita.dmd@gmail.com>
2024-02-13 15:15:12 +0300

    Feat: add layout of Login view

---
+++ iChat/Extensions/ButtonFormView.swift
@@ -0,0 +1,39 @@
+//
+//  ButtonFormView.swift
+//  iChat
+//
+//  Created by nik on 13.02.2024.
+//
+
+import UIKit
+
+class ButtonFormView: UIView {
+    
+    init(label: UILabel, button: UIButton) {
+        super.init(frame: .zero)
+        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(label)
+        addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: topAnchor),
+            label.leadingAnchor.constraint(equalTo: leadingAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20),
+            button.leadingAnchor.constraint(equalTo: leadingAnchor),
+            button.trailingAnchor.constraint(equalTo: trailingAnchor),
+            button.heightAnchor.constraint(equalToConstant: 60),
+            bottomAnchor.constraint(equalTo: button.bottomAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
